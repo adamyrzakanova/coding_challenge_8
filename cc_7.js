030b4f6d7086f1cd581d59a804921262ecec64b0
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-16 23:24:05 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -17,3 +17,13 @@ const calculateDiscount = function (price, discountRate) {
   };
   calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
   calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
+
+
+  // task 3
+const calculateServiceFee = (amount, serviceType) => {
+    let feeRate = serviceType === "Premium" ? 0.15 : serviceType === "Standard" ? 0.1 : 0.05;
+    let serviceFee = amount * feeRate;
+    console.log(`Service Fee: $${serviceFee.toFixed(2)}`);
+  };
+  calculateServiceFee(200, "Premium"); // Expected output: "Service Fee: $30.00"
+  calculateServiceFee(500, "Standard"); // Expected output: "Service Fee: $50.00"
